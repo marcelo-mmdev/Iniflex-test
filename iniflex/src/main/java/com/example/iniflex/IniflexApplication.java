b07d4b2567041d7cf0128cdc2f421032f6981768
Marcelo Rodrigues <85708828+marcelo-mmdev@users.noreply.github.com>
2023-07-15 19:51:47 -0300

    start project Iniflex test

---
+++ iniflex/src/main/java/com/example/iniflex/IniflexApplication.java
@@ -0,0 +1,13 @@
+package com.example.iniflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IniflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IniflexApplication.class, args);
+	}
+
+}
