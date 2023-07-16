812f8c0ad9da175dd8fdd532756eb9124ccb4cc4
Marcelo Rodrigues <85708828+marcelo-mmdev@users.noreply.github.com>
2023-07-15 21:18:14 -0300

    Organização da estrutura utilizada e Organização do Readme

--- iniflex/src/main/java/com/example/iniflex/project/test/IniflexApplication.java
+++ iniflex/src/main/java/com/example/iniflex/project/test/IniflexApplication.java
@@ -1,4 +1,4 @@
-package com.example.iniflex;
+package com.example.iniflex.project.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +8,9 @@ public class IniflexApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IniflexApplication.class, args);
+		System.out.println("*****************************************************");
+		System.out.println("Aplicação Rodando com Sucesso!!! ");
+		System.out.println("*****************************************************");
 	}
 
 }
